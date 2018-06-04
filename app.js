8f63f8a1fd337b89b219ba7e0978c07bc8e36627
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-06-04 09:24:29 +0200

    Ending challenge lesson 1
    [FA-77]

--- app.js
+++ app.js
@@ -1,54 +1,65 @@
-var express = require('express'),
-    app = express(),
-    engines = require('consolidate'),
-    bodyParser = require('body-parser'),
-    MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+const express = require('express');
+const engines = require('consolidate');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const app = express();
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Handler for internal server errors
 function errorHandler(err, req, res, next) {
-    console.error(err.message);
-    console.error(err.stack);
-    res.status(500).render('error_template', { error: err });
+  console.error(err.message);
+  console.error(err.stack);
+  res.status(500).render('error_template', { error: err });
 }
 
-MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+const getClient = () => {
+  let client = { db: undefined };
+  const insideGetClient = async() => {
+    if (client.db === undefined) {
+      console.log('Calculating client');
+      client.db = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+    }
+    return client.db;
+  };
+  return insideGetClient;
+};
 
-    assert.equal(null, err);
-    console.log("Successfully connected to MongoDB.");
+const getCachedClient = getClient();
 
-    app.get('/', function(req, res, next) {
-        res.render('add_movie', {});
-    });
-    
-    app.post('/add_movie', function(req, res, next) {
-        var title = req.body.title;
-        var year = req.body.year;
-        var imdb = req.body.imdb;
-        
-        if ((title == '') || (year == '') || (imdb == '')) {
-            next('Please provide an entry for all fields.');
-        } else {
-            db.collection('movies').insertOne(
-                { 'title': title, 'year': year, 'imdb': imdb },
-                function (err, r) {
-                    assert.equal(null, err);
-                    res.send("Document inserted with _id: " + r.insertedId);
-                }
-            );
-        }
-    });
-    
-    app.use(errorHandler);
-    
-    var server = app.listen(3000, function() {
-        var port = server.address().port;
-        console.log('Express server listening on port %s.', port);
-    });
-    
+
+app.get('/', function(req, res, next) {
+  res.render('add_movie', {});
 });
+
+app.post('/add_movie', async(req, res, next) => {
+  var title = req.body.title;
+  var year = req.body.year;
+  var imdb = req.body.imdb;
+
+  if ((title === '') || (year === '') || (imdb === '')) {
+    next('Please provide an entry for all fields.');
+  } else {
+    try {
+      const client = await getCachedClient();
+      const result = await client.db('video').collection('movies').insertOne(
+        { 'title': title, 'year': year, 'imdb': imdb });
+      res.send('Document inserted with _id: ' + result.insertedId);
+    } catch (e) {
+      next(e);
+    }
+  }
+});
+
+app.use(errorHandler);
+
+var server = app.listen(3000, function() {
+  var port = server.address().port;
+  console.log('Express server listening on port %s.', port);
+});
+
